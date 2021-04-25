aae899b036f8d7bc83765027a6ea0e6df70d898a
rajthilakam <rajthilakam@gmail.com>
2021-04-24 23:23:54 -0700

    Added json for products

---
+++ books.js
@@ -0,0 +1,59 @@
+const booksPage = document.querySelector(".products")
+console.log(booksPage)
+    
+class Products {
+    async getProducts() {
+      try {
+      let result = await fetch('http://localhost:3000/books.json'); 
+      let data = await result.json();
+      console.log(data)
+      let products = data.books;
+      return products
+    } catch(error){
+      console.log(error)
+    }
+    }
+  }
+
+class UI {
+    displayProducts(products) {
+        console.log(products)
+        let result = '';
+        products.forEach(product => {
+            result += `
+            
+            <div class="row">
+            <div class="col">
+                <div class="card mb-5" style="max-width: 1240px;">
+                  <div class="row no-gutters">
+                    <div class="col-md-4">
+                      <img src=${product.image} class="card-img" alt="Dress">
+                    </div>
+                    <div class="col-md-8">
+                      <div class="card-body">
+                        <h2 class="card-title">${product.title}</h2><br>
+                        <h5 class="card-text">${product.description}</h5><br>
+                        <div class="row row-content">
+                          <div class="col-6 item-price">
+                            <h5 id="item-price">$${product.price}</h5>
+                          </div>
+                          <div class="col-6 ">
+                            <button type="button" id="addCart" data-id=${product.sys.id} class="btn addCart">Add to Cart</button>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>             
+            </div>
+          </div>  `            
+        });
+        booksPage.innerHTML = result;    
+    }        
+}  
+
+  document.addEventListener('DOMContentLoaded',() => {
+    const products = new Products()
+    const ui = new UI()
+    products.getProducts().then(products => ui.displayProducts(products))
+  })
